2a923f867ade982624cd526fa38c0f04c48f82de
github username <github email address>
2017-09-19 13:41:05 -0400

    fixed up for master

--- oo-index.js
+++ oo-index.js
@@ -5,24 +5,4 @@ class Unicorn {
     this.health = 1000
   }
 
-  attack() {
-    return "'ATTAAAAAACK!''"
-  }
-
-  defend() {
-    return "You put sunglasses on."
-  }
-
-  heal() {
-    return "You feel refreshed after yoga."
-  }
-
-  breakdance() {
-    return "It's really spinning class"
-  }
-
-  eatCronut() {
-    return "You've gained 1000 health points."
-  }
-
 }
